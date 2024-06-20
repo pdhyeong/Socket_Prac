47e90d624d53c8ee3cfeb12b86dc5ae9c769f5e8
Ubuntu <ubuntu@ip-172-31-33-0.ap-northeast-2.compute.internal>
2024-06-20 21:13:54 +0900

    Check Addr and Network flow

---
+++ Hello_Socket/addr_test.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <arpa/inet.h>
+
+int main(int argc, char *argv[]){
+
+	char *addr1 = "1.2.3.4";
+	char *addr2 = "1.2.3.256";
+
+	unsigned long conv_addr = inet_addr(addr1);
+
+	if(conv_addr == INADDR_NONE)
+		printf("ERROR occured \n");
+	else
+		printf("Network ordered integer addr : %#lx \n", conv_addr);
+
+	conv_addr = inet_addr(addr2);
+
+	if(conv_addr == INADDR_NONE)
+		printf("ERROR occured \n\n");
+	else
+		printf("Network ordered integer addr : %#lx \n\n", conv_addr);
+
+
+	return 0;
+}
