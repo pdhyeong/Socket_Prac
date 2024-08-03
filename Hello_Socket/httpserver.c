476abc849ed3cb72f70473cc9e3f9accd52b6a30
Dohyeong Park <slstls218@gmail.com>
2024-08-03 17:34:05 +0900

    Fix Http ERROR

--- Hello_Socket/httpserver.c
+++ Hello_Socket/httpserver.c
@@ -9,134 +9,175 @@
 #define BUF_SIZE 1024
 #define SMALL_BUF 100
 
-void *request_handler(void *arg);
-void send_data(FILE *fp,char *ct,char *file_name);
-char* content_type(char *file);
-void error_handling(char *message);
+void* request_handler(void* arg);
+void send_data(FILE* fp, char* ct, char* file_name);
+char* content_type(char* file);
+void send_error(FILE* fp);
+void error_handling(char* message);
 
+int main(int argc, char *argv[])
+{
+	int serv_sock, clnt_sock;
+	struct sockaddr_in serv_adr, clnt_adr;
+	socklen_t clnt_adr_size;
+	char buf[BUF_SIZE];
+	pthread_t t_id;	
 
-int main(int argc,char *argv[]){
-    int serv_sock,clnt_sock;
-    struct sockaddr_in serv_adr,clnt_adr;
-    int clnt_adr_size;
-    char buf[BUF_SIZE];
+	if(argc!=2) {
+		printf("Usage : %s <port>\n", argv[0]);
+		exit(1);
+	}
+	
+	serv_sock = socket(PF_INET, SOCK_STREAM, 0);
+	if(serv_sock == -1)
+		error_handling("socket() error");
 
-    pthread_t t_id;
+	memset(&serv_adr, 0, sizeof(serv_adr));
+	serv_adr.sin_family = AF_INET;
+	serv_adr.sin_addr.s_addr = htonl(INADDR_ANY);
+	serv_adr.sin_port = htons(atoi(argv[1]));
 
-    if(argc != 2){
-        printf("Usage : %s <port> \n",argv[0]);
-        exit(1);
-    }
-    serv_sock = socket(PF_INET,SOCK_STREAM,0);
-    memset(&serv_adr,0,sizeof(serv_adr));
-    serv_adr.sin_family = AF_INET;
-    serv_adr.sin_addr.s_addr = htonl(INADDR_ANY);
-    serv_adr.sin_port = htons(atoi(argv[1]));
+	if(bind(serv_sock, (struct sockaddr*)&serv_adr, sizeof(serv_adr)) == -1)
+		error_handling("bind() error");
+	if(listen(serv_sock, 20) == -1)
+		error_handling("listen() error");
 
-    if(bind(serv_sock,(struct sockaddr*)&serv_adr,sizeof(serv_adr)) == -1){
-        error_handling("Bind ERROR");
-    }
-    if(listen(serv_sock,5) == -1){
-        error_handling("Listen ERROR");
-    }
-    while(1){
-        clnt_adr_size = sizeof(clnt_adr);
-        clnt_sock = accept(serv_sock,(struct sockaddr *)&clnt_adr,&clnt_adr_size);
-        printf("Conntected Request : %s %d \n",inet_ntoa(clnt_adr.sin_addr),(clnt_adr.sin_port));
-        pthread_create(&t_id,NULL,request_handler,&clnt_sock);
-        pthread_detach(t_id);
-    }
-    close(serv_sock);
-    return 0;
+	while(1)
+	{
+		clnt_adr_size = sizeof(clnt_adr);
+		clnt_sock = accept(serv_sock, (struct sockaddr*)&clnt_adr, &clnt_adr_size);
+		if(clnt_sock == -1)
+			error_handling("accept() error");
+		printf("Connection Request : %s:%d\n", 
+			inet_ntoa(clnt_adr.sin_addr), ntohs(clnt_adr.sin_port));
+		
+		int* sock_ptr = malloc(sizeof(int));
+		if (sock_ptr == NULL) {
+			error_handling("malloc() error");
+		}
+		*sock_ptr = clnt_sock;
+		pthread_create(&t_id, NULL, request_handler, sock_ptr);
+		pthread_detach(t_id);
+	}
+	close(serv_sock);
+	return 0;
 }
 
-void *request_handler(void *arg){
-    int clnt_sock = *((int *)arg);
-    char req_line[SMALL_BUF];
-    FILE *clnt_read;
-    FILE *clnt_write;
+void* request_handler(void *arg)
+{
+	int clnt_sock = *((int*)arg);
+	free(arg);
+	char req_line[SMALL_BUF];
+	FILE* clnt_read;
+	FILE* clnt_write;
+	
+	char method[10];
+	char ct[15];
+	char file_name[30];
+  
+	clnt_read = fdopen(clnt_sock, "r");
+	clnt_write = fdopen(dup(clnt_sock), "w");
+	if(clnt_read == NULL || clnt_write == NULL) {
+		send_error(clnt_write);
+		if (clnt_read) fclose(clnt_read);
+		if (clnt_write) fclose(clnt_write);
+		return NULL;
+	}
+	
+	if (fgets(req_line, SMALL_BUF, clnt_read) == NULL) {
+		send_error(clnt_write);
+		fclose(clnt_read);
+		fclose(clnt_write);
+		return NULL;
+	}
 
-    char method[10];
-    char ct[15];
-    char file_name[30];
+	if(strstr(req_line, "HTTP/") == NULL)
+	{
+		send_error(clnt_write);
+		fclose(clnt_read);
+		fclose(clnt_write);
+		return NULL;
+	}
+	
+	strcpy(method, strtok(req_line, " /"));
+	strcpy(file_name, strtok(NULL, " /"));
+	strcpy(ct, content_type(file_name));
+	if(strcmp(method, "GET") != 0)
+	{
+		send_error(clnt_write);
+		fclose(clnt_read);
+		fclose(clnt_write);
+		return NULL;
+	}
 
-    clnt_read = fdopen(clnt_sock,"r");
-    clnt_write = fdopen(dup(clnt_sock),"w");
-
-    fgets(req_line, SMALL_BUF,clnt_read);
-    if(strstr(req_line, "HTTP/") == NULL){
-        send_error(clnt_write);
-        fclose(clnt_read);
-        fclose(clnt_write);
-        return;
-    }
-    strcpy(method, strtok(req_line, " /"));
-    strcpy(file_name, strtok(NULL, " /"));
-    strcpy(ct, content_type(file_name));
-
-    if(strcmp(method, "GET") != 0){
-        send_error(clnt_write);
-        flcose(clnt_read);
-        flcose(clnt_write);
-        return ;
-    }
-    fclose(clnt_read);
-    send_data(clnt_write, ct, file_name);
-}
-void send_data(FILE *fp,char* ct, char* file_name){
-    char protocal[] = "HTTP/1.0 200 OK\n";
-    char server[] = "Server: Linux Webserver \r\n";
-    char cnt_len[]  = "Content0length:2048\r\n";
-    char cnt_type[SMALL_BUF];
-    char buf[BUF_SIZE];
-    FILE * send_file;
-
-    sprintf(cnt_type,"Content-type :%s\r\n\r\n",ct);
-    send_file=fopen(file_name,"r");
-    if(send_file == NULL){
-        send_error(fp);
-        return ;
-    }
-    fputs(protocal,fp);
-    fputs(server,fp);
-    fputs(cnt_len,fp);
-    fputs(cnt_type,fp);
-
-    while(fets(buf,BUF_SIZE,send_file) != NULL){
-        fputs(buf,fp);
-        fflush(fp);
-    }
-    fflush(fp);
-    fclose(fp);
+	fclose(clnt_read);
+	send_data(clnt_write, ct, file_name); 
+    return NULL;
 }
 
-char *content_type(char *file){
-    char extension[SMALL_BUF];
-    char file_name[SMALL_BUF];
+void send_data(FILE* fp, char* ct, char* file_name)
+{
+	char protocol[] = "HTTP/1.0 200 OK\r\n";
+	char server[] = "Server:Linux Web Server \r\n";
+	char cnt_type[SMALL_BUF];
+	char buf[BUF_SIZE];
+	FILE* send_file;
+	
+	sprintf(cnt_type, "Content-type:%s\r\n\r\n", ct);
+	send_file = fopen(file_name, "r");
+	if(send_file == NULL)
+	{
+		send_error(fp);
+		return;
+	}
 
-    strcpy(file_name,file);
-    strtok(file_name,".");
-    strcpy(extension, strtok(NULL,"."));
+	/* 응답 헤더 전송 */
+	fputs(protocol, fp);
+	fputs(server, fp);
+	fputs(cnt_type, fp);
 
-    if(!strcmp(extension, "html") || !strcmp(extension, "htm"))
-        return "text/html";
-    else   return "text/plain";
+	/* 파일 데이터 전송 */
+	while(fgets(buf, BUF_SIZE, send_file) != NULL) 
+	{
+		fputs(buf, fp);
+	}
+	fclose(send_file);
+	fclose(fp);
 }
-void send_error(FILE *fp){
-    char protocal[] = "HTTP/1.0 400 Bad Request\r\n";
-    char server[] = "Server: Linux Web Server\r\n";
-    char cnt_len[] = "Content-length:2048\r\n";
-    char cnt_type[] = "Content-type: text/html\r\n";
-    char content[] = "<html><head><title>NETWORK</title></head>""<body><font size=+5><br>Hello Http world</font></body></html>";
 
-    fputs(protocal,fp);
-    fputs(server,fp);
-    fputs(cnt_len,fp);
-    fputs(cnt_type,fp);
-    fflush(fp);
+char* content_type(char* file)
+{
+	char extension[SMALL_BUF];
+	char file_name[SMALL_BUF];
+	strcpy(file_name, file);
+	strtok(file_name, ".");
+	strcpy(extension, strtok(NULL, "."));
+	
+	if(!strcmp(extension, "html") || !strcmp(extension, "htm")) 
+		return "text/html";
+	else
+		return "text/plain";
 }
-void error_handling(char *message){
-    fputs(message,stderr);
-    fputs('\n',stderr);
-    exit(1);
+
+void send_error(FILE* fp)
+{	
+	char protocol[] = "HTTP/1.0 400 Bad Request\r\n";
+	char server[] = "Server:Linux Web Server \r\n";
+	char cnt_type[] = "Content-type:text/html\r\n\r\n";
+	char content[] = "<html><head><title>NETWORK</title></head>"
+	       "<h1>Request Falut!!!!</h1><body><font size=30><br>Again Request please"
+		   "</font></body></html>";
+
+	fputs(protocol, fp);
+	fputs(server, fp);
+	fputs(cnt_type, fp);
+	fputs(content, fp);
+	fclose(fp);
+}
+
+void error_handling(char* message)
+{
+	fputs(message, stderr);
+	fputc('\n', stderr);
+	exit(1);
 }
